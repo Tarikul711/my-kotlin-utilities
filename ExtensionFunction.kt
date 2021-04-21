de005e89bcb2dc5a60a430c6ee3ab608f2a8c7fa
tarikul711 <tarikul711@gmail.com>
2021-04-21 16:11:40 +0600

    Extension function for keyboard show and hide

--- ExtensionFunction.kt
+++ ExtensionFunction.kt
@@ -8,4 +8,23 @@ object ExtensionFunction {
         visibility = View.GONE
     }
 
+    /**
+     * Extension method to provide simpler access to {@link View#getResources()#getString(int)}.
+     */
+    fun View.getString(stringResId: Int): String = resources.getString(stringResId)
+
+    fun View.showKeyboard() {
+        val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        this.requestFocus()
+        imm.showSoftInput(this, 0)
+    }
+    
+    fun View.hideKeyboard(): Boolean {
+        try {
+            val inputMethodManager = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            return inputMethodManager.hideSoftInputFromWindow(windowToken, 0)
+        } catch (ignored: RuntimeException) {
+        }
+        return false
+    }
 }    
