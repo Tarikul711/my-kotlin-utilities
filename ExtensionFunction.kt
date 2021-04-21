cf24d7d5ccbc745393f25fab2334ddb859d1e509
tarikul711 <tarikul711@gmail.com>
2021-04-21 16:25:55 +0600

    Added Fragment related extension function

--- ExtensionFunction.kt
+++ ExtensionFunction.kt
@@ -1,6 +1,6 @@
 object ExtensionFunction {
 
-    fun View.show() : View {
+    fun View.show(): View {
         if (visibility != View.VISIBLE) {
             visibility = View.VISIBLE
         }
@@ -11,21 +11,21 @@ object ExtensionFunction {
         visibility = View.GONE
     }
 
-    fun View.remove() : View {
+    fun View.remove(): View {
         if (visibility != View.GONE) {
             visibility = View.GONE
         }
         return this
     }
 
-    fun View.hide() : View {
+    fun View.hide(): View {
         if (visibility != View.INVISIBLE) {
             visibility = View.INVISIBLE
         }
         return this
     }
 
-    fun View.toggleVisibility() : View {
+    fun View.toggleVisibility(): View {
         if (visibility == View.VISIBLE) {
             visibility = View.INVISIBLE
         } else {
@@ -69,13 +69,15 @@ object ExtensionFunction {
     }
 
 
-
-
     // ----------------------------------Context ---------------------------------------------------
 
     fun Context.getColorCompat(color: Int) = ContextCompat.getColor(this, color)
-    fun Context?.toast(text: CharSequence, duration: Int = Toast.LENGTH_LONG) = this?.let { Toast.makeText(it, text, duration).show() }
-    fun Context?.toast(@StringRes textId: Int, duration: Int = Toast.LENGTH_LONG) = this?.let { Toast.makeText(it, textId, duration).show() }
+    fun Context?.toast(text: CharSequence, duration: Int = Toast.LENGTH_LONG) =
+        this?.let { Toast.makeText(it, text, duration).show() }
+
+    fun Context?.toast(@StringRes textId: Int, duration: Int = Toast.LENGTH_LONG) =
+        this?.let { Toast.makeText(it, textId, duration).show() }
+
     fun Context.getInteger(@IntegerRes id: Int) = resources.getInteger(id)
     fun Context.getBoolean(@BoolRes id: Int) = resources.getBoolean(id)
     fun Context.getColor(@ColorRes id: Int) = ContextCompat.getColor(this, id)
@@ -106,6 +108,7 @@ object ExtensionFunction {
             return false
         }
     }
+
     fun Context.email(email: String, subject: String = "", text: String = ""): Boolean {
         val intent = intent(ACTION_SENDTO) {
             data = Uri.parse("mailto:")
@@ -119,6 +122,7 @@ object ExtensionFunction {
         }
         return false
     }
+
     fun Context.makeCall(number: String): Boolean {
         try {
             val intent = Intent(ACTION_CALL, Uri.parse("tel:$number"))
@@ -128,6 +132,7 @@ object ExtensionFunction {
             return false
         }
     }
+
     fun Context.sendSms(number: String, text: String = ""): Boolean {
         try {
             val intent = intent(ACTION_VIEW, Uri.parse("sms:$number")) {
@@ -139,6 +144,29 @@ object ExtensionFunction {
             return false
         }
     }
-    fun Context.rate(): Boolean = browse("market://details?id=$packageName") or browse("http://play.google.com/store/apps/details?id=$packageName")
+
+    fun Context.rate(): Boolean =
+        browse("market://details?id=$packageName") or browse("http://play.google.com/store/apps/details?id=$packageName")
+
+
+    //---------------------------------- Fragment -------------------------------------------------
+
+    fun Fragment?.toast(text: CharSequence, duration: Int = Toast.LENGTH_LONG) = this?.let { activity.toast(text, duration) }
+    fun Fragment?.toast(@StringRes textId: Int, duration: Int = Toast.LENGTH_LONG) = this?.let { activity.toast(textId, duration) }
+    fun SupportFragment?.toast(text: CharSequence, duration: Int = Toast.LENGTH_LONG) = this?.let { activity.toast(text, duration) }
+    fun SupportFragment?.toast(@StringRes textId: Int, duration: Int = Toast.LENGTH_LONG) = this?.let { activity.toast(textId, duration) }
+    fun Fragment.browse(url: String, newTask: Boolean = false) = activity.browse(url, newTask)
+    fun SupportFragment.browse(url: String, newTask: Boolean = false) = activity.browse(url, newTask)
+    fun Fragment.share(text: String, subject: String = "") = activity.share(text, subject)
+    fun SupportFragment.share(text: String, subject: String = "") = activity.share(text, subject)
+    fun Fragment.email(email: String, subject: String = "", text: String = "") = activity.email(email, subject, text)
+    fun SupportFragment.email(email: String, subject: String = "", text: String = "") = activity.email(email, subject, text)
+    fun Fragment.makeCall(number: String) = activity.makeCall(number)
+    fun SupportFragment.makeCall(number: String) = activity.makeCall(number)
+    fun Fragment.sendSms(number: String, text: String = "") = activity.sendSms(number, text)
+    fun SupportFragment.sendSms(number: String, text: String = "") = activity.sendSms(number, text)
+    fun Fragment.hideSoftKeyboard() {
+        activity?.hideSoftKeyboard()
+    }
 
 }    
